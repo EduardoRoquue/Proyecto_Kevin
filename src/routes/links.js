bd5163c1332eb270e17c34f44a51baf941bb3106
EduardoRoquue <eduardoroque450@gmail.com>
2021-11-07 01:06:14 -0600

    Funcion de editar links 1:53:29

--- src/routes/links.js
+++ src/routes/links.js
@@ -30,4 +30,23 @@ router.get('/delete/:id', async (req, res) => {
     res.redirect('/links');
 });
 
+router.get('/edit/:id', async (req, res) => {
+    const {id} = req.params;
+    const links = await pool.query('SELECT * FROM links WHERE id = ?', [id]);
+    res.render('links/edit', {link: links[0]});
+});
+
+router.post('/edit/:id', async (req, res) => {
+    const {id} = req.params;
+    const {title, descripcion, url} = req.body;
+    const newLink = {
+        title,
+        descripcion,
+        url
+    };
+    console.log(newLink);
+    await pool.query('UPDATE links set ? WHERE id = ?', [newLink, id]);
+    res.redirect('/links');
+});
+
 module.exports = router;
